332c1e97a6f3e2e5b12467f8504280f4c23e525c
Davide Galassi <davxy@datawok.net>
2025-08-14 18:06:42 +0200

    Improve decode.py script

--- scripts/decode.py
+++ scripts/decode.py
@@ -4,6 +4,9 @@ from jam_types import Struct, Header, OpaqueHash, ByteArray, ByteSequence, Vec, 
 from jam_types import spec, n
 import json
 import argparse
+import os
+import re
+import sys
 
 class TrieKey(ByteArray):
     element_count = 31
@@ -162,10 +165,24 @@ def main():
     type_help += " * Message: fuzzer protocol message (with no length prefix)\n"
     type_help += " * WireMessage: fuzzer protocol message (with length prefix)\n"
     
-    parser.add_argument('type', choices=type_mapping.keys(), help=type_help)
+    parser.add_argument('type', nargs='?', choices=type_mapping.keys(), help=type_help)
     
     args = parser.parse_args()
     
+    if not args.type:
+        # Infer type from filename
+        filename = os.path.basename(args.filename)
+        if filename == 'report.bin':
+            inferred_type = 'Report'
+        elif filename == 'genesis.bin':
+            inferred_type = 'Genesis'
+        elif re.match(r'^\d{8}\.bin$', filename):
+            inferred_type = 'TraceStep'
+        else:
+            inferred_type = 'Message'
+        print(f"Warning: No type specified, attempting to decode as {inferred_type} based on filename", file=sys.stderr)
+        args.type = inferred_type
+
     decode_type = type_mapping[args.type]   
     convert_to_json(args.filename, decode_type)
 
